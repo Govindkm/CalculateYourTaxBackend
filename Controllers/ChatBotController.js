a25394233813498671321727e152cec028a7ef09
Govind Mishra <35376412+Govindkm@users.noreply.github.com>
2023-05-12 14:48:57 +0530

    testing

--- Controllers/ChatBotController.js
+++ Controllers/ChatBotController.js
@@ -5,7 +5,7 @@ const chatBot = new SingletonChatBot().getInstance();
 
 router.post('/askQuestion', async (req, res) => {
     const question = req.body.question;
-    const prompt = " Based on given context answer the question, if you don't find anything relevant, appologize and ask for another question. Question: "+question;
+    const prompt = " Based on given context answer the question, if you don't find anything relevant, appologize and ask for another question. Add reference cases in case of application queries Question: "+question;
     const response = await chatBot.askQuestion(prompt);
     res.json(response);
 });
