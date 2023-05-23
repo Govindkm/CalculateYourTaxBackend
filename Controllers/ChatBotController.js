2ea01672275592776dd7bb4583b14b5837276847
Govind Kumar Mishra <govindkm.sv@gmail.com>
2023-05-23 21:26:14 +0530

    testing

--- Controllers/ChatBotController.js
+++ Controllers/ChatBotController.js
@@ -5,7 +5,7 @@ const chatBot = new SingletonChatBot().getInstance();
 
 router.post('/askQuestion', async (req, res) => {
     const question = req.body.question;
-    const prompt = " Based on given context answer the question, if you don't find anything relevant, appologize and ask for another question. Provide resolutions but don't copy paste confedential data and do not mix two different triage cases. Question: "+question;
+    const prompt = question;
     const response = await chatBot.askQuestion(prompt);
     res.json(response);
 });
