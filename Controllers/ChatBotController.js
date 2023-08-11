dce731208031b3c3ad173873306558843b72e0bb
Govind Kumar Mishra <govindkm.sv@gmail.com>
2023-08-11 18:11:08 +0530

    Revert "Adding chathistory for better conversation"
    This reverts commit 1f961175372c53ab2625bff3b39d96ced6c9fcc8.

--- Controllers/ChatBotController.js
+++ Controllers/ChatBotController.js
@@ -5,9 +5,8 @@ const chatBot = new SingletonChatBot().getInstance();
 
 router.post('/askQuestion', async (req, res) => {
     const question = req.body.question;
-    const chatHistory = req.body.chatHistory;
     const prompt = question;
-    const response = await chatBot.askQuestion(prompt, chatHistory);
+    const response = await chatBot.askQuestion(prompt);
     res.json(response);
 });
 
--- LangchainLLm/index.js
+++ LangchainLLm/index.js
@@ -1,140 +1,105 @@
-const path = require("path");
-const { OpenAI } = require("langchain/llms");
-const dotenv = require("dotenv");
-const { OpenAIEmbeddings } = require("langchain/embeddings");
-const { HNSWLib } = require("langchain/vectorstores");
-const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
-const { TextLoader } = require("langchain/document_loaders");
-const {
-  loadQAStuffChain,
-  loadQARefineChain,
-  ConversationalRetrievalQAChain,
-} = require("langchain/chains");
-const { BufferMemory } = require("langchain/memory");
-const fs = require("fs/promises");
-const logger = require("../Middlewares/logging");
+const path = require('path');
+const { OpenAI } = require('langchain/llms');
+const dotenv = require('dotenv');
+const { OpenAIEmbeddings } = require('langchain/embeddings');
+const { HNSWLib } = require('langchain/vectorstores');
+const { RecursiveCharacterTextSplitter } = require('langchain/text_splitter');
+const { TextLoader } = require('langchain/document_loaders');
+const { loadQAStuffChain } = require('langchain/chains');
+const fs = require('fs/promises');
+const logger = require('../Middlewares/logging');
+
 
 class GPTChatBot {
-  #model;
-  #chainA;
-  #object;
-  #vectorStore;
-
-  CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT = `Given the following conversation and a follow up question, return the conversation history excerpt that includes any relevant context to the question if it exists and rephrase the follow up question to be a standalone question.
-Chat History:
-{chat_history}
-Follow Up Input: {question}
-Your answer should follow the following format:
-\`\`\`
-Our customer use this tool to query details. Provided the chat history excerpt with some context and standalone question give correct answer to the user question based on context provided.
-Use the following pieces of context to answer the users question.
-If you are not able to find answer then ask for more details, don't try to make up an answer.
-----------------
-<Relevant chat history excerpt as context here>
-Standalone question: <Rephrased question here>
-\`\`\`
-Your answer:`;
-
-  constructor() {
-    if (process.env.NODE_ENV !== "production") {
-      dotenv.config({ path: "../../.env" });
-    } else {
-      dotenv.config({ path: "./environments/production.env" });
-    }
-    this.#model = new OpenAI({
-      openAIApiKey: process.env.OPEN_API_KEY,
-      temperature: 0.7,
-      verbose: true,
-    });
-    this.#chainA = loadQAStuffChain(this.#model);
-    this.directory = path.join(__dirname, "data");
-    this.#vectorStore = HNSWLib.load(
-      this.directory,
-      new OpenAIEmbeddings({ openAIApiKey: process.env.OPEN_API_KEY })
-    ).then((vector) => {
-      this.#vectorStore = vector;
-      this.chainA = ConversationalRetrievalQAChain.fromLLM(
-        this.#model,
-        this.#vectorStore.asRetriever(),
-        {
-          verbose: true,
-          memory: new BufferMemory({
-            memoryKey: "chat_history",
-            returnMessages: true,
-          }),
-          questionGeneratorTemplate: this.CUSTOM_QUESTION_GENERATOR_CHAIN_PROMPT
+    #model;
+    #chainA;
+    #object;
+    constructor() {
+        if (process.env.NODE_ENV !== 'production') {
+            dotenv.config({ path: '../../.env' });
         }
-      );
-    });
-  }
+        else {
+            dotenv.config({ path: './environments/production.env' });
+        }
+        this.#model = new OpenAI({ openAIApiKey: process.env.OPEN_API_KEY, temperature: 0.7 });
+        this.#chainA = loadQAStuffChain(this.#model);
+        this.directory = path.join(__dirname, 'data');
+        this.chainA = loadQAStuffChain(this.#model);
 
-  async createVectorEmbeddings() {
-    console.log("Loading text files...");
-    const loader = new TextLoader("./textfiles/Text1.txt");
-    const docs = await loader.load();
-    console.log("Text files loaded.");
-
-    console.log("SPlitting text files...");
-    const textSplietter = new RecursiveCharacterTextSplitter({
-      chunkSize: 500,
-      chunckOverlap: 100,
-    });
-
-    const splittedDocs = await textSplietter.splitDocuments(docs);
-    console.log("Text files splitted.");
-    // console.log({splittedDocs});
-
-    console.log("Creating vector store...");
-    const vectorStore = await HNSWLib.fromDocuments(
-      splittedDocs,
-      new OpenAIEmbeddings({ openAIApiKey: process.env.OPEN_API_KEY })
-    );
-    await vectorStore.save(this.directory);
-    console.log("Vector store created.");
-    return vectorStore;
-  }
-
-  async pathExists(path) {
-    try {
-      await fs.access(path);
-      return true;
-    } catch (err) {
-      console.log(err);
-      return false;
     }
-  }
 
-  async loadVectorEmbeddings() {
-    console.log("Loading vector store...");
+    async createVectorEmbeddings() {
 
-    const loadedVectorStore = await HNSWLib.load(
-      this.directory,
-      new OpenAIEmbeddings({ openAIApiKey: process.env.OPEN_API_KEY })
-    );
-    console.log("Vector store loaded.");
-    return loadedVectorStore;
-  }
+        console.log("Loading text files...");
+        const loader = new TextLoader("./textfiles/Text1.txt");
+        const docs = await loader.load();
+        console.log("Text files loaded.");
 
-  async askQuestion(question, chat_history = null) {
-    try {
-      if (await this.pathExists(this.directory)) {
-        await this.loadVectorEmbeddings();
-      } else {
-        await this.createVectorEmbeddings();
-      }
-      const resp = await this.chainA.call({
-        question,
-        chat_history,
-      });
-      console.log(resp);
-      return resp;
-    } catch (err) {
-      logger.error(err);
+        console.log("SPlitting text files...");
+        const textSplietter = new RecursiveCharacterTextSplitter({
+            chunkSize: 500,
+            chunckOverlap: 100,
+        });
 
-      return "Error occured while processing your request. Please try again later.";
+        const splittedDocs = await textSplietter.splitDocuments(docs);
+        console.log("Text files splitted.");
+        // console.log({splittedDocs});
+
+        console.log("Creating vector store...");
+        const vectorStore = await HNSWLib.fromDocuments(splittedDocs, new OpenAIEmbeddings({ openAIApiKey: process.env.OPEN_API_KEY }));
+        await vectorStore.save(this.directory);
+        console.log("Vector store created.");
+        return vectorStore;
     }
-  }
+
+    async pathExists(path) {
+        try {
+            await fs.access(path);
+            return true;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+
+    async loadVectorEmbeddings() {
+        console.log("Loading vector store...");
+
+        const loadedVectorStore = await HNSWLib.load(
+            this.directory,
+            new OpenAIEmbeddings({ openAIApiKey: process.env.OPEN_API_KEY })
+        );
+        console.log("Vector store loaded.");
+        return loadedVectorStore;
+    }
+
+    async askQuestion(question) {
+
+        try {
+            if (await this.pathExists(this.directory)) {
+                var vectorStore = await this.loadVectorEmbeddings();
+            } else {
+                var vectorStore = await this.createVectorEmbeddings();
+            }
+
+            const docs = await vectorStore.similaritySearch(question, 3);
+            console.log({ docs });
+            let query = `Our customer use this tool to query details. Provided the context and query give correct answer to the user query based on context provided. If you are not able to find answer then ask for more details. Here is the query: ${question}`;
+            const resp = await this.chainA.call({
+                input_documents: docs,
+                question: query,
+            });
+
+            return resp;
+        } catch (err) {
+            logger.error(err);
+
+            return "Error occured while processing your request. Please try again later.";
+        }
+
+    }
 }
+
 
 // const chainA = loadQAStuffChain(model);
 
@@ -143,6 +108,8 @@ Your answer:`;
 //     output: process.stdout
 // });
 
+
+
 // async function main() {
 //     if (await pathExists(directory)) {
 //         var vectorStore = await loadVectorEmbeddings();
@@ -150,7 +117,11 @@ Your answer:`;
 //         var vectorStore = await createVectorEmbeddings();
 //     }
 
+
 //     let question = "When was new regime announced prior to budget 2023?";
+
+
+
 
 //     const docs = await vectorStore.similaritySearch(question, 5);
 //     console.log({ docs });
@@ -165,15 +136,18 @@ Your answer:`;
 // }
 
 class SingletonChatBot {
-  constructor() {
-    if (!SingletonChatBot.instance) {
-      SingletonChatBot.instance = new GPTChatBot();
+    constructor() {
+        if (!SingletonChatBot.instance) {
+            SingletonChatBot.instance = new GPTChatBot();
+        }
     }
-  }
 
-  getInstance() {
-    return SingletonChatBot.instance;
-  }
+    getInstance() {
+        return SingletonChatBot.instance;
+    }
 }
 
-module.exports = { SingletonChatBot };
+module.exports = { SingletonChatBot }
+
+
+
